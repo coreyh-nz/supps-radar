e79be3068aa8d54ee623b41441319a7e04489f84
Corey Hines <cbh344@outlook.com>
2026-06-26 12:35:51 +1200

    chore: initial backend kotlin multi-module project

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,12 @@
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "supps-radar-backend"
